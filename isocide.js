88c7c8093ad4722535a89ef0b20a917428dc4fe1
Sebastian Ramadan <plebbyastian@gmail.com>
2019-03-18 18:28:23 +1000

    Added the beginnings of an editor/IDE
    You might want to copy/paste into this. I'm well aware of the selection bug. It's annoying me, too. I'll fix it soon.

---
+++ isocide.js
@@ -0,0 +1,77 @@
+with (document)
+{ with (body = createElement('body'))
+  { with (appendChild(createElement('div')))
+    { with (attributes)
+      { className = 'file';
+        contentEditable = true;
+      }
+      oninput = _ =>
+                { var token = phase[3](phase[2](phase[1](innerText[Symbol.iterator]())))
+                    , state = ({ 'diff_search': (ot, ot_distance, it, it_distance, token_list, tail_value, tail_type) =>
+                                                  ot.value[ot_distance] == undefined
+                                                    ? ot.done
+                                                      ? token_list.concat({ 'type':  tail_type || ot.type
+                                                                          , 'value': it.slice(0, it_distance)
+                                                                          , 'put_by': 'diff'
+                                                                          })
+                                                      : state['diff_search'](token.next(), 0, it.slice(it_distance), 0
+                                                                                         , token_list.concat({ 'type':  tail_type || ot.type
+                                                                                                             , 'value': it.slice(0, it_distance)
+                                                                                                             , 'put_by': 'diff'
+                                                                                                             }), '')
+                                                    : state[ot.value[ot_distance] == it[it_distance] ? 'diff_search' : 'same_search'](ot, ot_distance + 1, it, it_distance + 1, token_list, tail_value + it[it_distance], tail_type || ot.type)
+                               , 'same_search': (ot, ot_distance, it, it_distance, token_list, tail_value, tail_type) =>
+                                                  ot.value[ot_distance] == undefined
+                                                    ? ot.done
+                                                      ? token_list.concat({ 'type':  tail_type || ot.type
+                                                                          , 'value': it.slice(0, it_distance)
+                                                                          , 'put_by': 'same'
+                                                                          })
+                                                      : state['same_search'](token.next(), 0, it, it_distance, token_list, tail_value, tail_type || ot.type)
+                                                    : ot.value[ot_distance] == it[it_distance]
+                                                      ? state['diff_search'](ot, ot_distance, it.slice(it_distance), 0
+                                                                               , token_list.concat({ 'type':  tail_type || ot.type
+                                                                                                   , 'value': it.slice(0, it_distance)
+                                                                                                   , 'put_by': 'same'
+                                                                                                   }), '')
+                                                      : state['same_search'](ot, ot_distance, it, it_distance + 1, token_list, tail_value, tail_type || ot.type)
+                               })
+                    , element = state.diff_search(token.next(), 0, innerText, 0, [], '').map(expr =>
+                                                                                               (elem =>
+                                                                                               { with (elem)
+                                                                                                 { className = expr.type;
+                                                                                                   innerText = expr.value;
+                                                                                                 }
+                                                                                                 return elem;
+                                                                                               })(document.createElement('span')));
+                  var selection = window.getSelection();
+                  console.log(selection);
+                  while (firstChild)
+                    removeChild(firstChild);
+                  element.forEach(elem => appendChild(elem))
+                };
+    }
+    with (appendChild(createElement('div')))
+    { with (attributes)
+      { className = 'file_name';
+        contentEditable = true;
+      }
+      
+    }
+    with (appendChild(createElement('select')))
+    { with (attributes)
+      { className = 'file_name_list';
+        size = 2;
+      }
+      appendChild(createElement('option')).innerText = 'file_name #1';
+      appendChild(createElement('option')).innerText = 'file_name #2';
+      appendChild(createElement('option')).innerText = 'file_name #3';
+      var iterator = elem =>
+                     { while (elem.firstChild)
+                         elem.removeChild(elem.firstChild);
+                       elem.appendChild(selectedOptions[0].firstChild.cloneNode());
+                     };
+      oninput = _ => Array.from(document.getElementsByClassName('file_name')).forEach(iterator)
+    }
+  }
+}
